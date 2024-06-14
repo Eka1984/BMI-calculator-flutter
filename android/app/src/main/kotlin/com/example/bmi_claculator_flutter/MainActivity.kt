8e7857c3156c99332922f3176867c1fcf91e7974
Katja <katja.korzneva@gmail.com>
2024-06-14 13:00:36 +0300

    Flutter theme, layout and custom widgets

---
+++ android/app/src/main/kotlin/com/example/bmi_claculator_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bmi_claculator_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+
+          appBarTheme: AppBarTheme(
+              color: Color(0xFF0A0E21),
+              foregroundColor: Color(0xFFE4E5EA),
+              elevation: 10,
+              shadowColor: Colors.black
+          ),
+          scaffoldBackgroundColor: Color(0xFF0F1337),
+
+      ),
+      home: InputPage(),
+    );
+  }
+}
+
